627e9df2a60a99075f7d9bded5239386e33a8d79
Hinako-Tanaka <youngbird1228@gmail.com>
2021-02-12 17:35:38 +0900

    initial commit

---
+++ signupForm.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>新規作成</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd; ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">ドラマ・映画ノート</a>
+        </div>
+    </nav>
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputNickname1" class="form-label">ニックネーム</label>
+            <input type="text" class="form-control" id="exampleInputNickname1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputUserid1" class="form-label">ユーザーID</label>
+            <input type="text" class="form-control" id="exampleInputUserid1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">パスワード</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">登録</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
